f3ea7f1df74391c5a06f816cd87416f5d254cfd2
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-06-02 00:10:47 +0300

    mouseover js add

--- jacascript.js
+++ jacascript.js
@@ -11,21 +11,21 @@ colorButton1.addEventListener('click', function() {
   body.style.backgroundColor = randomColor;
   
 });
-// colorButton2.addEventListener('mouseover', function() {
-//   const randomColor = getRandomColor();
+colorButton2.addEventListener('mouseover', function() {
+  const randomColor = getRandomColor();
 
-//   body.style.backgroundColor = randomColor;
-// });
+  body.style.backgroundColor = randomColor;
+});
 
-// // Rastgele renk üreten yardımcı fonksiyon
-// function getRandomColor() {
-//   const letters = '0123456789ABCDEF';
-//   let color = '#';
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+// Rastgele renk üreten yardımcı fonksiyon
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 
 
