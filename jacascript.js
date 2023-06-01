62d4afe0d9bbda209060fd05764883d804e512f3
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-06-02 00:09:35 +0300

    click js add

---
+++ jacascript.js
@@ -0,0 +1,32 @@
+// DOM elemanını seçme( querySelector yöntemi ile <body> elementini seçiyoruz)
+const colorButton = document.getElementById('colorButton');
+const body = document.querySelector('body');
+
+// event listener ekleme(  addEventListener ile bir tıklama olayı dinleyici ekliyoruz.)
+colorButton1.addEventListener('click', function() {
+  // Rastgele renk oluşturma(getRandomColor fonksiyonunu kullanarak rastgele bir renk oluşturuyoruz )
+  const randomColor = getRandomColor();
+
+  // Sayfa arka plan rengini değiştirme( <body> elementinin backgroundColor stil özelliğini bu rastgele renk ile değiştiriyoruz.)
+  body.style.backgroundColor = randomColor;
+  
+});
+// colorButton2.addEventListener('mouseover', function() {
+//   const randomColor = getRandomColor();
+
+//   body.style.backgroundColor = randomColor;
+// });
+
+// // Rastgele renk üreten yardımcı fonksiyon
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+
+
+
